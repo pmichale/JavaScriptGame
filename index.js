fb3ca6740cf87a7265515b2d656db3059f0d9df5
Petr Michalek <pmich@email.cz>
2023-05-01 13:18:36 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 64 * 16
+canvas.height = 64 * 9
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Player {
+    constructior() {
+        this.position = {
+            x: 100,
+            y: 100,
+        }
+
+        this.width = 100
+        this.height = 100
+        this.edges = {
+            bottom: this.position.y + this.height
+        }
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        if (this.edges.bottom < canvas.height) {
+            this.position.y++
+            this.edges.bottom = this.position.y + this.height
+        }
+    }
+}
+
+const player = new Player()
+
+
+//let bottom = y + height
+function animate() {
+    window.requestAnimationFrame(animate) 
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.draw()
+    player.update()
+}
+
+animate()
